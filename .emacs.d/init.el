23c41ca961cba6ad5db5af06b0ff8b9b6e28f193
Kazuya Hatano <k28@me.com>
2022-03-03 10:53:58 +0900

    update emacs settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,8 +6,7 @@
 
 ;; package settings
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; install packages
@@ -135,7 +134,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (plantuml-mode auto-complete))))
+ '(package-selected-packages '(plantuml-mode auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
